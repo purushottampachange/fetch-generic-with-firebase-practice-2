8dd336330394e45d8c6a299b6d7a0640ead51be3
purushottam pachange <purushottampachnage@gmail.com>
2025-11-25 15:55:25 +0530

    worked on fetch funtionality

---
+++ assets/js/app.js
@@ -0,0 +1,113 @@
+
+const cl = console.log;
+
+const blogForm = document.getElementById("blogForm");
+const title = document.getElementById("title");
+const content = document.getElementById("content");
+const userId = document.getElementById("userId");
+const submitBtn = document.getElementById("submitBtn");
+const updateBtn = document.getElementById("updateBtn");
+const blogContainer = document.getElementById("blogContainer");
+const spinner = document.getElementById("spinner");
+
+const baseURL = "https://post-task-xhr-default-rtdb.firebaseio.com/";
+
+const PostURL = "https://post-task-xhr-default-rtdb.firebaseio.com/blogs.json";
+
+const converArr = (obj) => {
+
+    let res = [];
+
+    for (const key in obj) {
+
+        res.unshift({ ...obj[key], id: key });
+    }
+
+    return res;
+}
+
+const SnackBar = (icon, msg) => {
+
+    Swal.fire({
+        icon: icon,
+        title: msg,
+        timer: 1500
+    })
+}
+
+const MakeAPICall = (apiURL, method, body) => {
+
+    spinner.classList.remove("d-none");
+
+    body = body ? JSON.stringify(body) : null;
+
+    let configObj = {
+
+        method: method,
+        body: body,
+        headres: {
+
+            "auth": "token from local storage",
+            "content-type": "application/json"
+        }
+    }
+
+    return fetch(apiURL, configObj)
+        .then(res => {
+            if (!res.ok) {
+
+                throw new Error("something went wrong");
+            } else {
+
+                return res.json();
+            }
+        })
+        .catch(err => {
+
+            SnackBar("error", err);
+        })
+
+}
+
+
+const Templating = (arr) => {
+
+    let res = arr.map(b => {
+
+        return `
+        
+                  <div class="card mb-4" id="${b.id}">
+                        <div class="card-header">
+                            <h5>${b.title}</h5>
+                        </div>
+                        <div class="card-body">
+                            <p>${b.content}</p>
+                        </div>
+                        <div class="card-footer d-flex justify-content-between">
+                            <button class="btn btn-sm btn-success" onclick = "onEdit(this)">Edit</button>
+                            <button class="btn btn-sm btn-danger" onclick = "onRemove(this)">Remove</button>
+                        </div>
+                    </div>
+          
+        `;
+    }).join("")
+
+    blogContainer.innerHTML = res;
+}
+
+const FetchData = () => {
+
+    MakeAPICall(PostURL, "GET", null)
+        .then(res => {
+            let data = converArr(res);
+            cl(data);
+            Templating(data);
+        })
+        .finally(() => {
+
+            spinner.classList.add("d-none");
+
+        })
+}
+
+FetchData();
